1d4a82287ff3fd7bb5e44ea6b3bd69ddf4a02e36
MehmetDgmci <mehmet.degirmenci@byteagenten.de>
2024-04-20 20:13:43 +0200

    implemented the solution and added the test cases

--- src/main/java/JadenCasingStrings/Solution.java
+++ src/main/java/JadenCasingStrings/Solution.java
@@ -1,2 +1,25 @@
-package JadenCasingStrings;public class Solution {
+package JadenCasingStrings;
+
+public class Solution {
+	public String toJadenCase(String phrase) {
+		if (phrase == null || phrase.length() == 0) {
+			return null;
+		}
+		StringBuilder result = new StringBuilder();
+		String[] words = phrase.split("\\s+");
+
+		for (String word : words) {
+			char initial = word.charAt(0);
+			if (Character.isUpperCase(initial)) {
+				result.append(word + " ");
+			} else {
+				initial = Character.toUpperCase(initial);
+				result.append(initial + word.substring(1) + " ");
+
+			}
+		}
+		return result.toString().trim();
+	}
+
+
 }
--- src/test/java/JadenCasingStrings/SolutionTest.java
+++ src/test/java/JadenCasingStrings/SolutionTest.java
@@ -1,4 +1,31 @@
+package JadenCasingStrings;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SolutionTest {
-  
+
+	private Solution testEnv;
+
+	@BeforeEach
+	void setUp() {
+		this.testEnv = new Solution();
+		
+	}
+	@Test
+	public void test() {
+		assertEquals("Most Trees Are Blue", testEnv.toJadenCase("most trees are blue"));
+	}
+
+	@Test
+	public void testNullArg() {
+		assertNull(testEnv.toJadenCase(null));
+	}
+
+	@Test
+	public void testEmptyArg() {
+		assertNull(testEnv.toJadenCase(""));
+	}
 }
